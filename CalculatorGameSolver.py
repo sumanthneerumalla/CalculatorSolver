b3ea846def9a112032ad0b65f3e90c433e86e5a7
Sumanth Neerumalla <sumanth42gold@gmail.com>
2018-04-29 19:54:26 -0400

    Should be able to add moves now

--- CalculatorGameSolver.py
+++ CalculatorGameSolver.py
@@ -1,12 +1,42 @@
 class Game():
     def __init__(self):
-        self.Actions = []
-        self.Start = None
+        self.Actions = {}
+        self.numMoves = 0
+        self.Value = None
         self.Goal = None
         self.Moves = None
+
     def AddNum(self,value):
+        self.Value += value
+
+    def SubNum(self,value):
+        self.Value -= value
+
+    def MultNum(self,value):
+        self.Value *= value
+
+    def DivNum(self,value):
+        self.Value /= value
+
+    def setValue(self,value):
+        self.Value = value
+
+    def setGoal(self,value):
+        self.Goal = value
+
+    def setMove(self,value):
+        self.Moves = value
 
     def addMove(self,moveName,moveValue):
+        self.numMoves += 1
         if moveName == "Add":
-            self.Actions.append(AddNum)
+            self.Actions[self.numMoves] = (self.AddNum,moveValue)
+        elif moveName == "Subtract":
+            self.Actions[self.numMoves] = (self.SubNum,moveValue)
+        elif moveName == "Multiply":
+            self.Actions[self.numMoves] = (self.MultNum,moveValue)
+        elif moveName == "Divide":
+            self.Actions[self.numMoves] = (self.DivNum,moveValue)
 
+    def printMoves(self):
+        print (self.Actions)
