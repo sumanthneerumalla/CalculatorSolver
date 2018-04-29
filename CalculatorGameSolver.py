02e4e32cc1d77690fd40c0ea83f8c19129e2241e
Sumanth Neerumalla <sumanth42gold@gmail.com>
2018-04-29 00:52:08 -0400

    Well, got started

---
+++ CalculatorGameSolver.py
@@ -0,0 +1,12 @@
+class Game():
+    def __init__(self):
+        self.Actions = []
+        self.Start = None
+        self.Goal = None
+        self.Moves = None
+    def AddNum(self,value):
+
+    def addMove(self,moveName,moveValue):
+        if moveName == "Add":
+            self.Actions.append(AddNum)
+
